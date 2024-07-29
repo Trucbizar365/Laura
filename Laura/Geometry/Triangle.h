63e7a93367824e799ae7ba026e210a682bc7aa55
jakubg05 <jakub.gordiak@gmail.com>
2024-07-29 07:28:13 +0200

    previous commit fix

---
+++ Laura/Geometry/Triangle.h
@@ -0,0 +1,42 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+namespace Laura
+{
+    /**
+     * @struct Triangle
+     * @brief A structure representing a triangle in 3D space.
+     *
+     * This structure contains the vertices and normals of a triangle,
+     * as well as the material properties of the triangle.
+     * It is used in the construction of 3D meshes for ray tracing.
+     */
+    struct Triangle
+    {
+        // vertices
+        glm::vec3 v1;       //offset 0   // alignment 16 // size 12 // total 12 bytes
+        float padding1;     //offset 12  // alignment 4  // size 4  // total 16 bytes
+        glm::vec3 v2;       //offset 16  // alignment 16 // size 12 // total 28 bytes
+        float padding2;     //offset 28  // alignment 4  // size 4  // total 32 bytes 
+        glm::vec3 v3;       //offset 32  // alignment 16 // size 12 // total 44 bytes
+        float padding3;     //offset 44  // alignment 4  // size 4  // total 48 bytes
+
+        // normals
+        glm::vec3 NA;       //offset 48  // alignment 16 // size 12 // total 60 bytes
+        float padding4;     //offset 60  // alignment 4  // size 4  // total 64 bytes
+        glm::vec3 NB;       //offset 64  // alignment 16 // size 12 // total 76 bytes
+        float padding5;     //offset 76  // alignment 4  // size 4  // total 80 bytes
+        glm::vec3 NC;       //offset 80  // alignment 16 // size 12 // total 92 bytes
+        float padding6;     //offset 92  // alignment 4  // size 4  // total 96 bytes
+
+        glm::vec3 centroid;             //offset 96  // alignment 16 // size 12 // total 108 bytes
+        float padding7;	                //offset 108 // alignment 4  // size 4  // total 112 bytes
+
+        RaytracingMaterial material;    //offset 112 // alignment 16 // size 32 // total 144 bytes
+
+
+        // overload the << operator to print the triangle
+        friend std::ostream& operator<<(std::ostream& os, const Triangle& triangle);
+    };
+}
+#endif // TRIANGLE_H
---
+++ Laura/renderer/Material.h
@@ -0,0 +1,15 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+namespace Laura {
+
+    struct RaytracingMaterial
+    {
+        glm::vec3 color;              // offset 0   // alignment 16 // size 12 // total 12 bytes
+        float emissionStrength;       // offset 12  // alignment 4  // size 4  // total 16 bytes
+        glm::vec3 emissionColor;      // offset 16  // alignment 16 // size 12 // total 28 bytes
+        float std140padding;          // offset 28  // alignment 4  // size 4  // total 32 bytes
+    };
+}
+
+#endif // MATERIAL_H
