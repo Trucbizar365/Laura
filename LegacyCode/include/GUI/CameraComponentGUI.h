90bb928c4344ebb4f59974c07274b55ddd017595
jakubg05 <jakub.gordiak@gmail.com>
2024-08-06 19:33:47 +0200

    Finally a working version which builds in x64-debug and x64-release

--- LegacyCode/include/GUI/CameraComponentGUI.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "CameraOld/CameraOld.h"
-#include <imgui.h>
-#include <unordered_map>
-
-void scheduleKeybindChange(Laura::Camera& camera, int& keybind);
-void component_cameraGUI(Laura::Camera& camera, bool& was_IMGUI_Input, bool disabled, bool& shouldAccumulate, bool& doPostProc, int& raysPerPixel, int& bouncesPerRay);
-void genCameraChangeKeybindSplashScreen();
